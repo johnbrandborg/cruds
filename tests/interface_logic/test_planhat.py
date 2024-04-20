8add6a4561fdf2dffa5c616d244fc5b838630776
John Brandborg <jbr@trackunit.com>
2024-04-20 18:01:13 +0200

    Rename interface_methods to interface_logic

---
+++ tests/interface_logic/test_planhat.py
@@ -0,0 +1,8 @@
+"""
+Tests for Planhat interface logic in CRUDs
+"""
+
+from unittest import mock
+import pytest
+
+import cruds.interface_logic.planhat
---
+++ tests/test_interface.py
@@ -0,0 +1,8 @@
+"""
+Tests for the main Interface in CRUDs
+"""
+
+from unittest import mock
+import pytest
+
+import cruds.interface
